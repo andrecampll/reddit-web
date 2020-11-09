80e697ef1b8c42a6ea581f74748feb4dc66c53b8
André <andrevictor50@gmail.com>
2020-11-09 09:34:42 -0300

    getting me query

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,12 @@
 import { Flex, Link } from '@chakra-ui/core';
 import NextLink from 'next/link';
+import { useMeQuery } from '../generated/graphql';
 
-interface NavBarProps {
-
-}
+interface NavBarProps {}
 
 export const NavBar: React.FC<NavBarProps> = ({}) => {
+  const [{ data, fetching }] = useMeQuery();
+
   return (
     <Flex bg='#7159c1' p={4} justifyContent={'space-between'}>
       <NextLink href='/login'>
