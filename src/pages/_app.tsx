f09c0bf6b44b899e7bdced0950c41226283ac329
André <andrevictor50@gmail.com>
2020-11-18 20:11:07 -0300

    urql caching

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,30 @@
 import { ThemeProvider, CSSReset, ColorModeProvider } from '@chakra-ui/core'
-import { createClient, Provider } from 'urql'
+import { cacheExchange } from '@urql/exchange-graphcache';
+import { createClient, dedupExchange, fetchExchange, Provider } from 'urql'
+import { MeDocument } from '../generated/graphql';
+import theme from '../theme';
 
 const client = createClient({
   url: 'http://localhost:3333/graphql',
   fetchOptions: {
     credentials: 'include',
   },
+  exchanges: [
+    dedupExchange,
+    cacheExchange({
+      updates: {
+        Mutation: {
+          login: (result, args, cache, info) => {
+            cache.updateQuery({ query: MeDocument }, data => ({
+              
+            }));
+          },
+        },
+      },
+    }),
+    fetchExchange,
+  ],
 });
-
-import theme from '../theme'
 
 function MyApp({ Component, pageProps }) {
   return (
